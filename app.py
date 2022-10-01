413b4d59f07d186cc4169110c1fa8829926ed3a6
Manisha Varshney <94631536+manisha-v@users.noreply.github.com>
2022-10-01 17:10:33 +0530

    Final Commit

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import pandas as pd
 
 app = Flask(__name__)
 regmodel = pickle.load(open('regmodel.pkl','rb'))
-scaler = pickle.load(open('scaling.pkl','rb'))
+scalar = pickle.load(open('scaling.pkl','rb'))
 
 @app.route('/')
 def home():
@@ -16,10 +16,17 @@ def predict_api():
     data = request.json['data']
     print(data)
     print(np.array(list(data.values())).reshape(1,-1))
-    new_data = scaler.transform(np.array(list(data.values())).reshape(1,-1))
+    new_data = scalar.transform(np.array(list(data.values())).reshape(1,-1))
     output = regmodel.predict(new_data)
     print(output[0])
     return jsonify(output[0])
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    data = [float(x) for x in request.form.values()]
+    final_data = scalar.transform(np.array(list(data)).reshape(1,-1))
+    output = regmodel.predict(final_data)[0]
+    return render_template('home.html',prediction_text='Predicted Price is {}'.format(round(output,6)))
 
 if __name__ == "__main__":
     app.run(debug=True)
